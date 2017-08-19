ccc7771b2d4447587c8855c136a6d69c98428dc3
Li Ma <li.ma@duke.edu>
2017-08-19 11:37:22 -0400

    update default values and names of arguments.

--- R/lips_hd_pred.R
+++ R/lips_hd_pred.R
@@ -1,4 +1,4 @@
-lips_hd_pred = function( y.vec, x.mat, T = 10, kstep = 0, alpha = NA, method = "g", nparticle = 100, rho0=0.5, rho0.method ="beta-binomial",rho.prop=0.1,p.mix=6,resample.param=0,n.top.vars=5){
+lips_hd_pred = function( Y, X, T = NULL, kstep = 0, alpha = NULL, method = "g", nparticle = 100, rho0=0.5, rho0.method ="beta-binomial",rho.prop=0.1,p.mix=6,resample.param=0,n.top.vars=5){
   ## p.mix is the mixing probability for boostrap filter
   ## p.mix = 1 --> always boostrap filter
   ## p.mix = 0 --> always k-step filter
@@ -9,17 +9,18 @@ lips_hd_pred = function( y.vec, x.mat, T = 10, kstep = 0, alpha = NA, method = "
   ## p.mix = 5 --> SSS style proposal: selection probabilities for the top MAXVAR variables evenly distributed
   ## p.mix = 6 --> k-step filter (same as 0) implemented using reordering in the selection step
 
-  nobs = length(y.vec)
-  p = ncol(x.mat)
+  nobs = length(Y)
+  p = ncol(X)
   kstep = min(c(kstep,T), na.rm=TRUE)
-  T = min(p,T);
+  if (is.null(T)) T = p;
+  else  T = min(p,T);
 
 
 
   if (method == "g") method.int = as.integer(0)
   if (method == "hyper-g") method.int = as.integer(1)
 
-  if (is.na(alpha)) {
+  if (is.null(alpha)) {
     if (method == "g") alpha = nobs ## unit information g-prior
     if (method == "hyper-g") alpha = 3
   }
@@ -28,13 +29,13 @@ lips_hd_pred = function( y.vec, x.mat, T = 10, kstep = 0, alpha = NA, method = "
   if (rho0.method == "exp") rho0.method.int = as.integer(1)
   if (rho0.method == "beta-binomial") rho0.method.int = as.integer(2)
 
-  colnames(x.mat) = as.character(1:ncol(x.mat))
+  colnames(X) = as.character(1:ncol(X))
 
-  x.mat.centered <- apply(x.mat, 2, scale, scale=FALSE, center=TRUE)
-  y.vec.centered = y.vec - mean(y.vec)
+  X.centered <- apply(X, 2, scale, scale=FALSE, center=TRUE)
+  Y.centered = Y - mean(Y)
 
 
-  ans = .Call('modelsel_pf_hd_pred_C', y.vec.centered, x.mat.centered, p, as.integer(T), as.integer(kstep), as.numeric(alpha), method.int, as.integer(nparticle), as.numeric(rho0), rho0.method.int, as.numeric(rho.prop), as.numeric(p.mix),as.numeric(resample.param),as.integer(min(n.top.vars,T)))
+  ans = .Call('modelsel_pf_hd_pred_C', Y.centered, X.centered, p, as.integer(T), as.integer(kstep), as.numeric(alpha), method.int, as.integer(nparticle), as.numeric(rho0), rho0.method.int, as.numeric(rho.prop), as.numeric(p.mix),as.numeric(resample.param),as.integer(min(n.top.vars,T)))
 
   names(ans)=c("logweights","ave_incl_prob","size","logBF","models")
 
